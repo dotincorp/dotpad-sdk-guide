f04db6eca9ea0c386bb528937d22914256ea02ad
dotincorporation <103158861+dotincorporation@users.noreply.github.com>
2023-02-14 08:53:34 +0900

    [Windows] update dll
    v2.1.10.1

--- Windows/64bit/dot_pad_sdk.h
+++ Windows/64bit/dot_pad_sdk.h
@@ -27,6 +27,7 @@ extern "C"
 		DOTPADSDK_API DOT_PAD_SDK_ERROR DOT_PAD_DISPLAY_DATA_PART(uint8_t* data, int len, int startIdx);
 		DOTPADSDK_API DOT_PAD_SDK_ERROR DOT_PAD_RESET_DISPLAY();
 		DOTPADSDK_API DOT_PAD_SDK_ERROR DOT_PAD_BRAILLE_DISPLAY(const wchar_t* strInput, int language);
+		DOTPADSDK_API DOT_PAD_SDK_ERROR DOT_PAD_SET_BRAILLE_LANGUAGE(const int languageCode, const int option);
 		DOTPADSDK_API DOT_PAD_SDK_ERROR DOT_PAD_BRAILLE_ASCII_DISPLAY(const char* brailleASCII);
 		DOTPADSDK_API DOT_PAD_SDK_ERROR DOT_PAD_RESET_BRAILLE_DISPLAY();
 		DOTPADSDK_API DOT_PAD_SDK_ERROR DOT_PAD_SEND_KEY(int nKeyCode);
@@ -35,6 +36,7 @@ extern "C"
 		DOTPADSDK_API DOT_PAD_SDK_ERROR DOT_PAD_GET_DISPLAY_INFO(int* width, int* height, int* braille);
 		DOTPADSDK_API DOT_PAD_SDK_ERROR DOT_PAD_REGISTER_KEY_CALLBACK(void(CALLBACK* cb)(int));
 		DOTPADSDK_API DOT_PAD_SDK_ERROR DOT_PAD_REGISTER_DISPLAY_CALLBACK(void(CALLBACK* cb)(void));
+		DOTPADSDK_API DOT_PAD_SDK_ERROR DOT_PAD_GET_DTMS_DATA(const char* url);
 #if !defined(DOT_PAD_SDK_DLL_EXPORTS)
 		// function type retrieved from DotPadSDK DLL
 		typedef DOT_PAD_SDK_ERROR(*DOT_PAD_INIT_FUNC)(int port_number);
@@ -45,6 +47,7 @@ extern "C"
 		typedef DOT_PAD_SDK_ERROR(*DOT_PAD_DISPLAY_DATA_PART_FUNC)(uint8_t* data, int len, int startIdx);
 		typedef DOT_PAD_SDK_ERROR(*DOT_PAD_RESET_DISPLAY_FUNC)();
 		typedef DOT_PAD_SDK_ERROR(*DOT_PAD_BRAILLE_DISPLAY_FUNC)(const wchar_t* strInput, int language);
+		typedef DOT_PAD_SDK_ERROR(*DOT_PAD_SET_BRAILLE_LANGUAGE_FUNC)(const int languageCode, const int option);
 		typedef DOT_PAD_SDK_ERROR(*DOT_PAD_BRAILLE_ASCII_DISPLAY_FUNC)(const char* brailleASCII);
 		typedef DOT_PAD_SDK_ERROR(*DOT_PAD_RESET_BRAILLE_DISPLAY_FUNC)();
 		typedef DOT_PAD_SDK_ERROR(*DOT_PAD_SEND_KEY_FUNC)(int nKeyCode);
@@ -53,6 +56,7 @@ extern "C"
 		typedef DOT_PAD_SDK_ERROR(*DOT_PAD_GET_DISPLAY_INFO_FUNC)(int* width, int* height, int* braille);
 		typedef DOT_PAD_SDK_ERROR(*DOT_PAD_REGISTER_KEY_CALLBACK_FUNC)(void(CALLBACK* cb)(int));
 		typedef DOT_PAD_SDK_ERROR(*DOT_PAD_REGISTER_DISPLAY_CALLBACK_FUNC)(void(CALLBACK* cb)(void));
+		typedef DOT_PAD_SDK_ERROR(*DOT_PAD_GET_DTMS_DATA_FUNC)(const char* url);
 #endif
 	}
 
